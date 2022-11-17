13f311cefdc69fe5d68cdea91effb0f3465d5d99
harry thomas <harryt1905@gmail.com>
2022-11-17 13:39:25 -0500

    minor changes to controller for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -29,11 +29,7 @@ class Admin::CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(
       :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
+      :category_id
     )
   end
 
